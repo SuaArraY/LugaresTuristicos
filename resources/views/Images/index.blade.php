f51f402ec062388a1fa2a6006fdd8f8fa157bd2a
Yossec SA <yosec.arratea22@gmail.com>
2022-11-13 11:06:48 -0500

    fin de la publicidad de imagenes - falta ingresar ciudades

--- resources/views/Images/index.blade.php
+++ resources/views/Images/index.blade.php
@@ -7,9 +7,7 @@
             <div class="card">
                 <img src="images/{{ $row->image }}" alt="Image">
                 <h4>Datos</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                 Expedita repudiandae asperiores modi nulla assumenda 
-                </p>
+                
                 <a href="">leer</a>
             </div>
             @endforeach
