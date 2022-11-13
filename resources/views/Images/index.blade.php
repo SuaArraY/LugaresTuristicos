83e7c4f8114810907cb03f58c26973fb468eb198
Yossec SA <yosec.arratea22@gmail.com>
2022-11-12 23:57:36 -0500

    diseño terminado del almacen de las ciudades

--- resources/views/Images/index.blade.php
+++ resources/views/Images/index.blade.php
@@ -9,8 +9,6 @@
                 <h4>Datos</h4>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                  Expedita repudiandae asperiores modi nulla assumenda 
-                 facere molestias voluptas blanditiis illum delectus,
-                 dicta vel. Ducimus perspiciatis illo iste cupiditate neque sed perferendis!
                 </p>
                 <a href="">leer</a>
             </div>
