88637074103146d830bfe00952c4ef35c1c904b9
Yossec SA <yosec.arratea22@gmail.com>
2022-11-12 23:36:39 -0500

    diseño para mostrar las ciudades

--- resources/views/Images/index.blade.php
+++ resources/views/Images/index.blade.php
@@ -2,9 +2,19 @@
 
 @section('content')
     
-    @foreach ($images as $row)
-        <img src="images/{{ $row->image }}" alt="Image">
-    @endforeach
-    
-
+        
+            @foreach ($images as $row)
+            <div class="card">
+                <img src="images/{{ $row->image }}" alt="Image">
+                <h4>Datos</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                 Expedita repudiandae asperiores modi nulla assumenda 
+                 facere molestias voluptas blanditiis illum delectus,
+                 dicta vel. Ducimus perspiciatis illo iste cupiditate neque sed perferendis!
+                </p>
+                <a href="">leer</a>
+            </div>
+            @endforeach
+        
+   
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!--link para conectar a la carpeta css -->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+    <!--link para las letras diseño-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
@@ -35,12 +42,11 @@
 
         <div class="wave" style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.99 C150.00,150.00 349.21,-49.99 500.00,49.99 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
     </header>
-    <main>
-        <section class="contenedor">
-            @yield('content')
-        </section>
-    </main>
 
+    <section class="container">
+        @yield('content')
+    </section>
+   
    
 </body>
 </html>
