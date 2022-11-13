0bc9849f4d582100aacb6ad002d6605031d1f7a8
Yossec SA <yosec.arratea22@gmail.com>
2022-11-13 17:34:01 -0500

    validacion de los campos del formulario y el cambio de idioma a la hora de mostrar los errores

--- resources/views/tarea/tareacreate.blade.php
+++ resources/views/tarea/tareacreate.blade.php
@@ -48,7 +48,17 @@
                 </div>
 
             </div>
+
         </form>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
         
    
 @endsection
